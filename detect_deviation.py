406e5a2ae6718168c115fc6e12913a3dd5f20d05
voygern <voygern@163.com>
2019-03-04 19:44:17 +0000

    SecondCommit

--- detect_deviation.py
+++ detect_deviation.py
@@ -4,6 +4,9 @@ import pandas as pd
 # script to test the basic function of tushare
 def test_script():
     # try to get history data of a stock
-    df = ts.get_hist_data(code='885738',start="20170501",stop="20170630",ktype="60")
+    #df = ts.get_h_data(code='000725',start="20170501",end="20170630",ktype="D")
+    df = ts.get_hist_data(code="000725",ktype="60")
     print(df.head())
-
+    print("content of df:")
+    print(df)
+test_script()
